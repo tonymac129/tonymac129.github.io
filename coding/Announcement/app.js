287e5c8c7515379595711ec6155502a2ae9ff16f
Tony Macaroni <141281444+tonymac129@users.noreply.github.com>
2023-11-11 12:01:59 -0500

    Delete coding directory

--- coding/Announcement/app.js
+++
@@ -1,9 +0,0 @@
-const annoucement = document.getElementById("annoucement");
-const close = document.getElementById("close");
-annoucement.classList.remove("not-appear");
-
-function notappear() {
-  annoucement.classList.add("not-appear");
-}
-
-close.addEventListener("click",notappear);
